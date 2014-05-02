ed0d24ba2b94703106742c0256f7b47ab22c1930
bleporini <bleporini@gmail.com>
2014-05-02 19:54:25 +0200

    Test configuration was not Ok for Tx management

--- src/test/java/jpasearch/TestApplication.java
+++ src/test/java/jpasearch/TestApplication.java
@@ -1,23 +1,28 @@
 package jpasearch;
 
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.instrument.classloading.InstrumentationLoadTimeWeaver;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * @author speralta
  */
 @Configuration
 @ComponentScan
+@EnableTransactionManagement
 public class TestApplication {
 
     @Bean
@@ -26,22 +31,24 @@ public class TestApplication {
     }
 
     @Bean
-    public PlatformTransactionManager transactionManager(DataSource dataSource) {
-        return new DataSourceTransactionManager(dataSource);
+    public PlatformTransactionManager transactionManager() {
+        return new JpaTransactionManager( entityManagerFactory() );
     }
 
     @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory(HibernateJpaVendorAdapter jpaVendorAdapter, DataSource dataSource) {
+    public EntityManagerFactory entityManagerFactory() {
         LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
-        entityManagerFactoryBean.setJpaVendorAdapter(jpaVendorAdapter);
-        entityManagerFactoryBean.setDataSource(dataSource);
+        entityManagerFactoryBean.setJpaVendorAdapter(jpaVendorAdapter());
+        entityManagerFactoryBean.setDataSource(dataSource());
         entityManagerFactoryBean.setPackagesToScan("jpasearch.domain");
         entityManagerFactoryBean.getJpaPropertyMap().put("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
         entityManagerFactoryBean.getJpaPropertyMap().put("hibernate.search.analyzer", "org.apache.lucene.analysis.fr.FrenchAnalyzer");
         entityManagerFactoryBean.getJpaPropertyMap().put("hibernate.search.default.directory_provider", "ram");
         entityManagerFactoryBean.getJpaPropertyMap().put("hibernate.search.default.worker.execution", "async");
         entityManagerFactoryBean.getJpaPropertyMap().put("hibernate.search.default.worker.execution", "3");
-        return entityManagerFactoryBean;
+        entityManagerFactoryBean.afterPropertiesSet();
+        entityManagerFactoryBean.setLoadTimeWeaver(new InstrumentationLoadTimeWeaver());
+        return entityManagerFactoryBean.getObject();
     }
 
     @Bean

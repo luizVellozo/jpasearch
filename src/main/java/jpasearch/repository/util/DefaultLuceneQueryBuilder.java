111f6995947ad3eae42cb9b7754974b6671a80ac
speralta <sebastien@peralta.fr>
2014-08-04 13:24:24 +0200

    fix: hibernate search query building

--- src/main/java/jpasearch/repository/util/DefaultLuceneQueryBuilder.java
+++ src/main/java/jpasearch/repository/util/DefaultLuceneQueryBuilder.java
@@ -8,6 +8,7 @@ import javax.inject.Singleton;
 
 import jpasearch.repository.query.selector.TermSelector;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.lucene.search.Query;
 import org.hibernate.search.jpa.FullTextEntityManager;
 import org.hibernate.search.query.dsl.BooleanJunction;
@@ -17,7 +18,7 @@ import org.hibernate.search.query.dsl.QueryBuilder;
 @Singleton
 public class DefaultLuceneQueryBuilder implements LuceneQueryBuilder {
 
-    private static final String SPACES_OR_PUNCTUATION = "\\p{Punct}|\\p{Blank}";
+    private static final String PUNCTUATION = "\\p{Punct}";
 
     @Override
     public <T> Query build(FullTextEntityManager fullTextEntityManager, TermSelector<T> termSelector, Class<? extends T> type) {
@@ -30,36 +31,27 @@ public class DefaultLuceneQueryBuilder implements LuceneQueryBuilder {
             BooleanJunction<?> termContext = builder.bool();
             for (String selected : termSelector.getSelected()) {
                 if (isNotBlank(selected)) {
-                    BooleanJunction<?> splitContext = builder.bool();
-                    for (String value : selected.split(SPACES_OR_PUNCTUATION)) {
-                        if (isNotBlank(value)) {
-                            BooleanJunction<?> valueContext = builder.bool();
-                            if (termSelector.getSearchSimilarity() != null) {
-                                valueContext.should(builder.keyword().fuzzy() //
-                                        .withEditDistanceUpTo(termSelector.getSearchSimilarity()) //
-                                        .onField(termSelector.getPath()) //
-                                        .matching(value).createQuery());
-                            }
-                            valueContext.should(builder.keyword() //
+                    String value = StringUtils.join(selected.split(PUNCTUATION), " ");
+                    if (isNotBlank(value)) {
+                        BooleanJunction<?> valueContext = builder.bool();
+                        if (termSelector.getSearchSimilarity() != null) {
+                            valueContext.should(builder.keyword().fuzzy() //
+                                    .withEditDistanceUpTo(termSelector.getSearchSimilarity()) //
                                     .onField(termSelector.getPath()) //
                                     .matching(value).createQuery());
-                            valueContext.should(builder.keyword().wildcard() //
-                                    .onField(termSelector.getPath()) //
-                                    .matching("*" + value + "*").createQuery());
-                            if (termSelector.isOrMode()) {
-                                splitContext.should(valueContext.createQuery());
-                            } else {
-                                splitContext.must(valueContext.createQuery());
-                            }
-                            hasTerms = true;
                         }
-                    }
-                    if (hasTerms) {
+                        valueContext.should(builder.keyword() //
+                                .onField(termSelector.getPath()) //
+                                .matching(value).createQuery());
+                        valueContext.should(builder.keyword().wildcard() //
+                                .onField(termSelector.getPath()) //
+                                .matching("*" + value + "*").createQuery());
                         if (termSelector.isOrMode()) {
-                            termContext.should(splitContext.createQuery());
+                            termContext.should(valueContext.createQuery());
                         } else {
-                            termContext.must(splitContext.createQuery());
+                            termContext.must(valueContext.createQuery());
                         }
+                        hasTerms = true;
                     }
                 }
             }
